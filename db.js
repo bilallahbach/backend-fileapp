621d791e06bea1ef6ce18131b6e876318bd950b1
bilal lahbach <lhbachbilal@gmail.com>
2025-10-27 11:25:12 +0100

    change

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  port: process.env.DB_PORT || 3306,
 });
 
-module.exports = pool.promise();
+module.exports = db;
